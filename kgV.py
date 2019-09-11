3a5bafaff3f9cb476dbf2cc6e6be095f74eac073
Jonathan <jonathan.schulze.de@gmail.com>
2019-09-11 16:19:20 +0200

    backup

--- kgV.py
+++ kgV.py
@@ -1,7 +1,6 @@
 #kleinstes gemeinsames Vielfaches
 
-prime_numbers = [2.0,3.0,5.0,7.0,11.0,13.0,17.0,19.0,23.0,29.0]#more than enough
-
+prime_numbers = [2.0,3.0,5.0,7.0,11.0,13.0,17.0,19.0,23.0,29.0,31.0]#more than enough
 def input_nenner(n):
     nenner = []
     for i in range(n):
@@ -9,25 +8,40 @@ def input_nenner(n):
     print(nenner)
     return nenner
 
+def append_to_kgV(kgV_list, single_list):
+    print(single_list)
+    for prime in single_list:
+        if prime not in kgV_list:
+            kgV_list[prime_numbers.index(prime)].append(prime)
+    return kgV_list
 def calculate_kgV(list):
     kgV_list = []
+    for n in range(len(prime_numbers)):
+        kgV_list.append([])
     for n in range(len(list)):
-        multibles_list = []
+        single_list = []
+        print(single_list)
         number = list[n]
         print("number:", number)
         i = 0
         while i < len(prime_numbers):
+            print(i)
             if number in prime_numbers:
-                multibles_list.append(number)
+                single_list.append(number)
+                print("break")
                 break
-            if (number/prime_numbers[i]).is_integer():
-                multibles_list.append(prime_numbers[i])
+            elif (number/prime_numbers[i]).is_integer():
+                single_list.append(prime_numbers[i])
                 number = number/prime_numbers[i]
                 print("yeah", number)
                 i = 0
             else:
                 i=+1
-        if count([])
-
+                print("+1")
+                if i > len(prime_numbers):
+                    print("ohno")
+        kgV_list = append_to_kgV(kgV_list, single_list)
     return kgV_list
+
+print(list)
 print(calculate_kgV(input_nenner(int(input("wie viele nenner?")))))
